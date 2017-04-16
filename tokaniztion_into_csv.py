edfa1a650a510bb4eea2d6d2433167ea7f16251e
Noura Hussein <nourahussein193@gmail.com>
2017-04-16 20:54:11 +0300

    Update tokaniztion_into_csv.py

--- tokaniztion_into_csv.py
+++ tokaniztion_into_csv.py
@@ -6,9 +6,10 @@
 import nltk
 import csv
 import pandas as pd
-file_content = open("test.txt").read()
-tokens = nltk.word_tokenize(file_content)
-df = pd.DataFrame(tokens, columns=["colummn"])
-cols.remove("coulmn")
-df.to_csv('list.csv', index=False)
+#enter your txt file path for input , and csv file for output
+def tokanzie_into_csv(input_file,output_file):
+  file_content = open(input_file).read()
+  tokens = nltk.word_tokenize(file_content)
+  df = pd.DataFrame(tokens, columns=["colummn"])
+  df.to_csv(output_file, index=False) 
 

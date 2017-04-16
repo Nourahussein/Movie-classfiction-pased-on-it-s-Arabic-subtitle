7e4c35efef50efb63b49d8a3b2f4531c552f23a7
Noura Hussein <nourahussein193@gmail.com>
2017-04-17 00:00:19 +0300

    Update tokaniztion_into_csv.py

--- tokaniztion_into_csv.py
+++ tokaniztion_into_csv.py
@@ -7,9 +7,9 @@ import nltk
 import csv
 import pandas as pd
 #enter your txt file path for input , and csv file for output
-def tokanzie_into_csv(input_file,output_file):
+def tokanzie_into_csv(input_file,output_file, str movie_name):
   file_content = open(input_file).read()
   tokens = nltk.word_tokenize(file_content)
-  df = pd.DataFrame(tokens, columns=["colummn"])
+  df = pd.DataFrame(tokens, columns=[movie_name])
   df.to_csv(output_file, index=False) 
 

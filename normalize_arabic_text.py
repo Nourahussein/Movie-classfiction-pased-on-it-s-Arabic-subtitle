f3b0299f557e45d6f8a26d3e28df5468e05a89ee
Noura Hussein <nourahussein193@gmail.com>
2017-04-18 00:58:24 +0300

    Update normalize_arabic_text.py

--- normalize_arabic_text.py
+++ normalize_arabic_text.py
@@ -4,18 +4,19 @@ import sys
 import argparse
 def normalize_arabic(input_file,output_file):
     file_content = open(input_file).read()
-    #normalize arabic text
+    #normalize arabic text replacing some chrachters
     file_content = re.sub("[إأآا]", "ا", file_content)
     file_content = re.sub("ى", "ي", file_content)
     file_content = re.sub("ؤ", "ء", file_content)
     file_content = re.sub("ئ", "ء", file_content)
     file_content = re.sub("ة", "ه", file_content)
     file_content = re.sub("گ", "ك", file_content)
-    output = open("output.txt", "w")
-
+    # writing text to output file 
+    output = open(output_file, "w")
+    
     output.write(file_content)
 
     output.close()
     
     return output
-normalize_arabic("input.txt", "output.txt")
+# example : normalize_arabic("input.txt", "output.txt")
